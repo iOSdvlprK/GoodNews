a3f74d7b5b335e27b1d498354c2f0add89459ce4
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-02-06 23:22:27 +0900

    setup the user interface

---
+++ GoodNews/Controllers/NewsListTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsListTableViewController.swift
+//  GoodNews
+//
+//  Created by joe on 2/6/24.
+//
+
+import UIKit
+
+class NewsListTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
